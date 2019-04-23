0b52c7d4990ca4cbc6591765b15100d364908a88
github username <github email address>
2019-04-22 23:54:43 -0400

    used devise for full authentication

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-  has_many :appointments
-  has_many :locations, through: :appointments
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :users do
     resource :appointments, only: [:new, :show, :index]
   end
+
+  root to: "user#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
