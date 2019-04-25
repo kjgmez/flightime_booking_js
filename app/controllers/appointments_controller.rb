fd72c633569b2ef5437f992e9c11513073203a3f
github username <github email address>
2019-04-24 21:54:45 -0400

    created seeds for coaches and locations

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,4 +3,21 @@ class AppointmentsController < ApplicationController
     @user = current_user
     @flights = @user.appointments
   end
+  def new
+    @appointment= Appointment.new
+  end
+  def show
+    @appointment = Appointment.find(params[:id])
+  end
+  def edit
+    @appointment = Appointment.find(params[:id])
+  end
+  def update
+    @appointment = Appointment.find(params[:id])
+  end
+  def delete
+    appointment = current_user.appointments.find(params[:id])
+    appointment ? appointment.destroy : redirect_to user_appointments_path(current_user), error: "The appointment does not exists"
+    redirect_to user_appointments_path(current_user)
+  end
 end
