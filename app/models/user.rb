b91b111227a92b5dd29c98d5d6ae6cda53ce8475
github username <github email address>
2019-04-29 23:19:43 -0400

    updates for omni in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  validates :level, numericality: {less_than_or_equal_to: 4}
+  validates :level, numericality: {less_than_or_equal_to: 4}, unless: :provider
 
   has_many :appointments
   has_many :locations, through: :appointments
@@ -12,8 +12,14 @@ class User < ApplicationRecord
          :omniauthable, :omniauth_providers => [:google_oauth2]
 
   def self.from_omniauth(auth)
+    binding.pry
     # Either create a User record or update it based on the provider (Google) and the UID
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.provider = auth.provider
+      user.uid = auth.uid
       user.token = auth.credentials.token
       user.expires = auth.credentials.expires
       user.expires_at = auth.credentials.expires_at
