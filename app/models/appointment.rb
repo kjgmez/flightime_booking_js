c9844e0080e5678ffbda394ef138ba196477680f
github username <github email address>
2019-04-29 21:36:09 -0400

    update models

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,9 @@ class Appointment < ApplicationRecord
   belongs_to :location
   belongs_to :coach
 
+  validates :user_id, :location_id, :arrival_time, presence: true
+  validates :minutes, numericality: {greater_than: 0}
+
   scope :ordered_by_arrival_time, -> { order(arrival_time: :asc) }
   # scope :ordered_by_level, -> { order(level: :desc) }
 
