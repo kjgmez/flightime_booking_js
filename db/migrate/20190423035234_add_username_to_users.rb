24f6fa71f21dbc04086c3e529864db28c221416a
github username <github email address>
2019-04-29 23:21:33 -0400

    updates for omni in migrations

--- db/migrate/20190423035234_add_username_to_users.rb
+++ db/migrate/20190423035234_add_username_to_users.rb
@@ -1,7 +1,5 @@
 class AddUsernameToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :user_name, :string
-    add_index :users, :user_name, unique: true
     add_column :users, :name, :string
     add_column :users, :balance, :integer
     add_column :users, :level, :integer
