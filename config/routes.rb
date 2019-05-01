ef5b62d4e49856f183444ac6fc2688757dc7b6b6
github username <github email address>
2019-05-01 14:53:15 -0400

    updates for deploy on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: {omniauth_callbacks: "callbacks", registrations: 'registrations'}
 
   resources :locations, only: [:index, :show] do
