7c1eea0f950102ed63b7e1c74547cabb2e7d855c
github username <github email address>
2019-05-01 19:38:08 -0400

    adding appointment migration

--- db/migrate/20190427170351_edit_column_minutes_to_appointment.rb
+++
@@ -1,5 +0,0 @@
-class EditColumnMinutesToAppointment < ActiveRecord::Migration[5.2]
-  def change
-    change_column :appointments, :minutes, :datetime
-  end
-end
--- db/migrate/20190501233332_create_appointments.rb
+++ db/migrate/20190501233332_create_appointments.rb
@@ -3,8 +3,9 @@ class CreateAppointments < ActiveRecord::Migration[5.2]
     create_table :appointments do |t|
       t.references :user, foreign_key: true
       t.references :location, foreign_key: true
+      t.references :coach, foreign_key: true
       t.datetime :arrival_time
-      t.integer :minutes
+      t.datetime :minutes
 
       t.timestamps
     end
