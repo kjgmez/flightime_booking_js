2376ee2e54a3d6bfb38cf5c041f1367696453732
github username <github email address>
2019-08-22 13:01:47 -0400

    fixed indexing

--- app/assets/javascripts/appointment.js
+++ app/assets/javascripts/appointment.js
@@ -1,32 +1,33 @@
-$(document).ready(() => {
-    $( "#booked_time_link" ).click(handleBookedTimeClick);
-});
-
-function handleBookedTimeClick(event) {
-    event.preventDefault();
-    let user = $(this).attr('data-user-id');
-    $.get("/users/" + user + "/appointments.json", response => {
-        response.forEach( appt => {
-            $("#show-appointments").innerHTML +=`<li> ${appt.location.name} </li>`
-        })
-
-    })
-
-}
-
 // $(document).ready(() => {
-//     $( "#booked_time" ).click(function(event) {
-//         event.preventDefault();
-//         let id = $(this).attr('data-id');
-//         let user = $(this).attr('user-id');
-//         let url = "/users/" + user + "/appointments/" + id;
-//         // debugger;
-//         fetch(url)
-//             .then(response => test(response.json()))
-//         // .then(data => {debugger})
-//         ;
-//     });
+//     $( "#booked_time_link" ).click(handleBookedTimeClick);
 // });
+//
+// function handleBookedTimeClick(event) {
+//     event.preventDefault();
+//     let user = $(this).attr('data-user-id');
+//     $.get("/users/" + user + "/appointments.json", function (response) {
+//         debugger;
+//         response.forEach( appt => {
+//             $("#show-appointments").innerHTML +=`<li> ${appt} </li>`
+//         })
+//
+//     })
+//
+// }
+
+$(document).ready(() => {
+    $( "#booked_time_link" ).click(function(event) {
+        event.preventDefault();
+        // debugger;
+        let user = $(this).attr('data-user-id');
+        fetch(`/users/${user}/appointments.json`)
+            .then(res => res.json())
+            .then(data => data.forEach( appt => {
+                $( "#booked_time_link" ).append(`<li>${appt["arrival_time"]}</li>`)
+            }))
+        ;
+    });
+});
 //
 // function test(data) {
 //     console.log(data)
